b48b66e99356dec36d7b511fce498114ceb3b4c3
mason park <joung4342@gmail.com>
2022-05-11 13:39:37 +0900

    State 업데이트 함수 구현

--- src/components/habit.jsx
+++ src/components/habit.jsx
@@ -1,20 +1,17 @@
 import React, {Component} from 'react';
 
 class Habit extends Component {
-    state = {
-        count : 0,
+    handleIncrement = () => {
+        this.props.onIncrement(this.props.habit);
+    };
+    handleDecrement = () => {
+        this.props.onDecrement(this.props.habit);
     };
 
-    handleIncrement = () =>{
-        //state 오브젝트 안에 있는 count를 증가 한뒤 state를 업데이트 한다.
-        // this.state.count +=1; 그냥 값을 변경하면 리엑트는 이 값이 변경되었는지 알 수 없다.
-        // 그래서 state값을 변경하고 싶으면 this.setState 함수를 사용
-        this.setState({count: this.state.count + 1});
+    handleDelete = () => {
+        this.props.onDelete(this.props.habit);
     };
-    handleDecrement = () =>{
-        const count = this.state.count - 1
-        this.setState({count: count < 0 ? 0 : count });
-    };
+
 
     render() {
         // console.log(this.props.habit);
@@ -29,7 +26,7 @@ class Habit extends Component {
                 <button className="habit-button habit-decrease" onClick={this.handleDecrement}>
                     <i className="fas fa-minus-square"></i>
                 </button>
-                <button className="habit-button habit-delete">
+                <button className="habit-button habit-delete" onClick={this.handleDelete}>
                     <i className="fas fa-trash"></i>
                 </button>
             </li>
--- src/components/habits.jsx
+++ src/components/habits.jsx
@@ -4,17 +4,72 @@ import Habit from "./habit";
 class Habits extends Component {
     state = {
         habits: [
-            {id:1,name: 'Reading', count: 0},
-            {id:2,name: 'Running', count: 0},
-            {id:3,name: 'Coding', count: 0},
+            {id: 1, name: 'Reading', count: 0},
+            {id: 2, name: 'Running', count: 0},
+            {id: 3, name: 'Coding', count: 0},
         ],
+    };
+    handleIncrement = (habit) => {
+        // console.log(`handleIncrement ${habit.name}`);
+        //1. habits 배열에서 원하는 인덱스 추출
+        //2. 해당하는 인덱스의 객체에서 내가 원하는 값만을 바꾸고 싶음
+        //3. 값을 바꾸고 다시 state 에 업데이트
+     /* my code
+        const targetIndex = this.state.habits.findIndex((item => item.id === habit.id));
+        let count = habit.count +1;
+        let copyArr = [...this.state.habits];
+        if (targetIndex !== -1) {
+            copyArr[targetIndex] = {...copyArr[targetIndex], count: count};
+        }
+        this.setState({habits:copyArr});*/
+        const habits = [...this.state.habits];
+        const index = habits.indexOf(habit);
+        habits[index].count ++ ;
+        this.setState({habits});    // key 와 value 과 같으면 생략 가능
+    };
+    handleDecrement = (habit) => {
+        // console.log(`handleDecrement ${habit.name}`);
+/*        const targetIndex = this.state.habits.findIndex((item => item.id === habit.id));
+        let copyArr = [...this.state.habits];
+        let count = habit.count -1;
+        if (count < 0) {
+            count = 0;
+        }
+        if (targetIndex !== -1) {
+            copyArr[targetIndex] = {...copyArr[targetIndex], count: count};
+        }
+        this.setState({habits:copyArr});*/
+        const habits = [...this.state.habits];
+        const index = habits.indexOf(habit);
+        const count = habits[index].count -1;
+        habits[index].count = count<0 ? 0 : count;
+        this.setState({habits});    // key 와 value 과 같으면 생략 가능
+
+
+    };
+
+    handleDelete = (habit) => {
+        // console.log(`handleDelete ${habit.name}`);
+       /* const targetIndex = this.state.habits.findIndex((item => item.id === habit.id));
+        let copyArr = [...this.state.habits];
+        if (targetIndex !== -1) {
+            copyArr.splice(targetIndex,1);
+        }
+        this.setState({habits:copyArr});*/
+        const habits  = this.state.habits.filter(item => item.id !== habit.id);
+        this.setState({habits});
     };
 
     render() {
         return (
             <ul>
                 {this.state.habits.map(habit =>
-                    (<Habit key={habit.id} habit={habit} />)
+                    (<Habit key={habit.id}
+                            habit={habit}
+                            onIncrement={this.handleIncrement}
+                            onDecrement={this.handleDecrement}
+                            onDelete={this.handleDelete}
+                    />)
                 )}
             </ul>
         );
