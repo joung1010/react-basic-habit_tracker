e280d57c701d553b6bf81697d5f560feed4f5c49
mason park <joung4342@gmail.com>
2022-05-15 13:47:19 +0900

    About function Component And Memo Function

--- src/components/addHabit.jsx
+++ src/components/addHabit.jsx
@@ -1,29 +1,30 @@
-import React, {PureComponent} from 'react';
-
-class AddHabit extends PureComponent {
-    inputRef = React.createRef();
-    formRef =React.createRef();
+import React, {memo } from 'react';
+//memo 함수 인자로 우리가 만든 컴포넌트 함수를 전달하면 PureComponent 와 같은 역할을 하게 된다.
+// rci 를 사용해서 빠르게 만들 수 있다.
+const AddHabit = memo((props) => {
+    const inputRef = React.createRef();
+    const formRef =React.createRef();
     //React는 바로 DOM 요소에 접근하지 않고
     // 필요할때는 React.createRef() 이용해서 멤버 변수를 정의한다음
     // 원하는 컴포넌트에 ref 속성으로 연결하면 된다.
 
-    handleSubmit = (event)=>{
+    const handleSubmit = (event)=>{
         event.preventDefault();
-        const habitName = this.inputRef.current.value;
-        habitName && this.props.onAddHabit(habitName);
+        const habitName = inputRef.current.value;
+        habitName && props.onAddHabit(habitName);
         // this.inputRef.current.value='';
-        this.formRef.current.reset();
+        formRef.current.reset();
     };
 
-    render() {
-        console.log('addHabit');
-        return (
-            <form ref={this.formRef} onSubmit={this.handleSubmit}>
-                <input ref={this.inputRef} type="text" className="add-input" placeholder="Habit"/>
-                <button className="add-button">Add</button>
-            </form>
-        );
-    }
-}
+
+
+    return (
+        <form ref={formRef} onSubmit={handleSubmit}>
+            <input ref={inputRef} type="text" className="add-input" placeholder="Habit"/>
+            <button className="add-button">Add</button>
+        </form>
+    );
+});
+
 
 export default AddHabit;
